cf49d32aab64fdff2ce1bead98b4cbd9258c760b
Bruno Silva <bruno.silva@edi-labs.com>
2020-07-07 20:54:57 -0300

    Directory organization

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,9 @@ require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') })
 const express   = require('express');
 const mongoose  = require('mongoose');
 
-// mongoose.connect(`mongodb//${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE_NAME}`, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
+const app       = express();
+const http      = require('http').Server(app);
+const io        = require('socket.io')(http);
 
 mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+'/'+process.env.DB_DATABASE_NAME, {
     useCreateIndex: true,
@@ -19,15 +16,20 @@ mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@
     useFindAndModify: false
 });
 
-
-const app = express();
-
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello World" });
+ 
+// cria uma rota para fornecer o arquivo index.html
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3333, () => {
+// sempre que o socketio receber uma conexão vai devoltar realizar o broadcast dela
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3333, () => {
     console.log("Server started");
 });
